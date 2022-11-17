3c9a9063e6a1e4321f4ebb6e0164aad10734e05f
WarBuggy <hovanbuu@gmail.com>
2022-11-17 23:34:34 +0700

    _ add letter on cenes cxtracted _ move into 1 mod More Biotech Letters

---
+++ MoreBiotechLetters/MBLSetting.cs
@@ -0,0 +1,28 @@
+﻿using Verse;
+
+namespace MoreBiotechLetters
+{
+    public class MBLSettings : ModSettings
+    {
+        public bool sendLetterGenesRegrowFinished;
+
+        public bool pauseGenesRegrowFinished;
+
+        public bool sendLetterGenesExtracted;
+
+        public bool pauseGenesExtracted;
+
+
+        public bool debug;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref sendLetterGenesRegrowFinished, "sendLetterGenesRegrowFinished", true, false);
+            Scribe_Values.Look(ref pauseGenesRegrowFinished, "pauseGenesRegrowFinished", true, false);
+            Scribe_Values.Look(ref sendLetterGenesExtracted, "sendLetterGenesExtracted", true, false);
+            Scribe_Values.Look(ref pauseGenesExtracted, "pauseGenesExtracted", true, false);
+            Scribe_Values.Look(ref debug, "debug", false, false);
+            base.ExposeData();
+        }
+    }
+}
--- MoreBiotechLetters/Properties/AssemblyInfo.cs
+++ MoreBiotechLetters/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LetterGenesRegrowFinish")]
+[assembly: AssemblyTitle("MoreBiotechLetters")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LetterGenesRegrowFinish")]
+[assembly: AssemblyProduct("MoreBiotechLetters")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a9fa085d-e414-49f4-a256-e0e2f490a17a")]
+[assembly: Guid("b32b35a1-b5f5-45b7-ac5b-c8662fb05fa3")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
